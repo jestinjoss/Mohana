a7ec72b42d2822abe8dd225f6db6a6419088d815
jestinjoss <158200909+jestinjoss@users.noreply.github.com>
2024-01-31 19:19:26 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,7 +1,7 @@
 def count_lines(content):
     print("No of lines: #")
-with open(r"E:/moni/testfile2.txt", 'r')as fp:
+with open(r"E:/moni/test.txt", 'r')as fp:
     lines = sum(1 for line in fp)
 print('total number of lines:',lines)
-file = open("E:/moni/testfile2.txt", "r")
+file = open("E:/moni/test.txt", "r")
 content=file.read()
